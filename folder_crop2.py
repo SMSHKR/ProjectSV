e89b88a2e7329e8eb367a70140ccde8fefaeddff
aikengsurakarn <54305221+aikengsurakarn@users.noreply.github.com>
2020-06-01 12:34:36 +0700

    Add files via upload

---
+++ folder_crop2.py
@@ -0,0 +1,30 @@
+import sys
+import cv2
+import numpy as np
+
+def crop(infile, outfile, thresh=127):
+
+    originalImage = cv2.imread(infile)
+    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, thresh, maxval=255, type=cv2.THRESH_BINARY)
+
+    # find where the signature is and make a cropped region
+    points = np.argwhere(blackAndWhiteImage==0) # find where the black pixels are
+    points = np.fliplr(points) # store them in x,y coordinates instead of row,col indices
+    x, y, w, h = cv2.boundingRect(points) # create a rectangle around those points
+    crop = grayImage[y:y+h, x:x+w] # create a cropped region of the gray image
+
+    # get the thresholded crop
+    (retval, thresh_crop) = cv2.threshold(crop, thresh, maxval=255, type=cv2.THRESH_BINARY)
+
+    # cv2.imshow('Cropped image', thresh_crop)
+    cv2.imwrite(outfile, thresh_crop)
+    
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+# arg py folder_crop.py .\name\
+for index in range(1, 11):
+    infile = '%s%s.jpg' % (sys.argv[1], str(index))
+    outfile = '%sc%s.jpg' % (sys.argv[1], str(index))
+    crop(infile, outfile, 100)
