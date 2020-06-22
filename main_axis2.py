9a739f811e368bdbd9a3774925721d41c5ad39d1
aikengsurakarn <54305221+aikengsurakarn@users.noreply.github.com>
2020-06-23 02:08:14 +0700

    Add files via upload

--- main_axis2.py
+++ main_axis2.py
@@ -6,13 +6,13 @@ import math
 from scipy import ndimage
 
 #loading our BW image
-img = cv2.imread("test2_all.jpg", 0)
+img = cv2.imread("test2_at_ed.jpg", 0)
 h, w = img.shape
 
 #From a matrix of pixels to a matrix of coordinates of non-black points.
 #(note: mind the col/row order, pixels are accessed as [row, col]
 #but when we draw, it's (x, y), so have to swap here or there)
-mat = np.argwhere(img != 0)
+mat = np.argwhere(img != 255)
 mat[:, [0, 1]] = mat[:, [1, 0]]
 mat = np.array(mat).astype(np.float32) #have to convert type for PCA
 
@@ -24,14 +24,14 @@ m, e = cv2.PCACompute(mat, mean = np.array([]))
 #and the secondary by 50
 center = tuple(m[0])
 endpoint1 = tuple(m[0] + e[0]*100)
-endpoint2 = tuple(m[0] + e[1]*50)
+endpoint2 = tuple(m[0] + e[1]*100)
 
 print(center)
 print(endpoint1)
 
 cv2.circle(img, center, 5, 128)
 # cv2.line(img, center, right, 128)
-# cv2.line(img, center, endpoint1, 0)
+cv2.line(img, center, endpoint1, 0)
 cv2.line(img, center, endpoint2, 128)
 
 delta0 = endpoint1[0]-center[0]
@@ -42,7 +42,8 @@ angle = math.atan2(delta0, delta1)
 angle = angle*180/math.pi
 print(angle)
 inv = cv2.bitwise_not(img)
-rotated = ndimage.rotate(inv, -angle)
+rotated = ndimage.rotate(inv, 90-angle)
 inv = cv2.bitwise_not(rotated)
+img = inv
 
-cv2.imwrite("test2_ma.jpg", inv)
+cv2.imwrite("test2_at_ed_ma.jpg", img)
